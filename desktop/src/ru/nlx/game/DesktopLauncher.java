8f852332b398ac59361a4fc0100b1cffb559a464
NevLax <qwegornikitin@gmail.com>
2022-07-01 20:47:19 +0400

    Initial commit

---
+++ desktop/src/ru/nlx/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package ru.nlx.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import ru.nlx.game.MyMainClass;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Dinozdets");
+		new Lwjgl3Application(new MyMainClass(), config);
+	}
+}
