3fff63cd3641cc51d6fd72ebb58ac6d8e803542b
NevLax <qwegornikitin@gmail.com>
2022-07-24 17:01:47 +0400

    better control

--- core/src/ru/nlx/game/Dino.java
+++ core/src/ru/nlx/game/Dino.java
@@ -1,14 +1,8 @@
 package ru.nlx.game;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-
-import java.util.Vector;
 
 public class Dino {
     MyAnimation wait;
@@ -17,22 +11,13 @@ public class Dino {
     CircleShape circle;
     FixtureDef fixtureDef;
     Fixture fixture;
-    boolean isRIGHT;
-    boolean isLEFT;
-    boolean isUP;
-    boolean isDOWN;
-    Vector2 move;
-    Vector2 buff;
-    Interpolation interpol;
     float koff;
+    DinoController control;
 
     public Dino(Texture texture, World world){
         wait = new MyAnimation(texture, 24, 24,0.25f);
-        move = new Vector2();
-        buff = new Vector2();
-        koff = 20f;
-
-        interpol = new Interpolation.SwingOut(0.5f);
+        koff = 50f;
+        control = new DinoController();
 
         bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.KinematicBody;
@@ -53,30 +38,8 @@ public class Dino {
     }
 
     public void draw(SpriteBatch batch){
-        isUP = Gdx.input.isKeyPressed(Input.Keys.W);
-        isDOWN = Gdx.input.isKeyPressed(Input.Keys.S);
-        isRIGHT = Gdx.input.isKeyPressed(Input.Keys.D);
-        isLEFT = Gdx.input.isKeyPressed(Input.Keys.A);
-
-        if (isUP){
-            move.set(0f, 1*koff);
-        } else if (isDOWN) {
-            move.set(0f, -1*koff);
-        } else if (isRIGHT) {
-            move.set(1*koff, 0f);
-        } else if (isLEFT) {
-            move.set(-1*koff, 0f);
-        }   else {
-            move.set(0f, 0f);
-        }
-
-//        buff.set(interpol.apply(move));
-        buff.set(interpol.apply(buff.x, move.x, Gdx.graphics.getDeltaTime()),
-                interpol.apply(buff.y, move.y, Gdx.graphics.getDeltaTime())
-        );
-
         wait.render(batch, body.getPosition());
-        body.setLinearVelocity(buff);
+        body.setLinearVelocity(control.control(koff));
     }
 
     public void dispose(){
---
+++ core/src/ru/nlx/game/DinoController.java
@@ -0,0 +1,77 @@
+package ru.nlx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.Vector2;
+
+public class DinoController {
+
+    float timeKoff;
+    Vector2 vector;
+    Vector2 buff;
+    Interpolation interpol;
+
+    public DinoController(){
+        timeKoff = 1f;
+        vector = new Vector2();
+        buff = new Vector2();
+        interpol = new Interpolation.SwingOut(0.2f);
+    }
+
+    public Vector2 control(float koff){
+
+        if (isUP() & isDOWN()) {
+            setX(0f);
+        }   else if (isUP()) {
+            setX(koff);
+        }   else if (isDOWN()) {
+            setX(-koff);
+        }   else setX(0f);
+
+        if (isRIGHT() & isLEFT()) {
+            setY(0f);
+        }   else if (isRIGHT()) {
+            setY(koff);
+        }   else if (isLEFT()) {
+            setY(-koff);
+        }   else setY(0f);
+
+        buff.set(interpol.apply(buff.x, vector.x, Gdx.graphics.getDeltaTime()*timeKoff),
+                interpol.apply(buff.y, vector.y, Gdx.graphics.getDeltaTime()*timeKoff));
+
+        return invert();
+    }
+
+    private boolean isUP(){
+        return Gdx.input.isKeyPressed(Input.Keys.W) |
+                Gdx.input.isKeyPressed(Input.Keys.UP);
+    }
+
+    private boolean isDOWN(){
+        return Gdx.input.isKeyPressed(Input.Keys.S) |
+                Gdx.input.isKeyPressed(Input.Keys.DOWN);
+    }
+
+    private boolean isRIGHT(){
+        return Gdx.input.isKeyPressed(Input.Keys.D) |
+                Gdx.input.isKeyPressed(Input.Keys.RIGHT);
+    }
+
+    private boolean isLEFT(){
+        return Gdx.input.isKeyPressed(Input.Keys.A) |
+                Gdx.input.isKeyPressed(Input.Keys.LEFT);
+    }
+
+    private void setX(float value){
+        vector.set(value, vector.y);
+    }
+
+    private void setY(float value){
+        vector.set(vector.x, value);
+    }
+
+    private Vector2 invert(){
+        return new Vector2(buff.y, buff.x);
+    }
+}
